7706b342e70fcf83749c3179c577a177e7419e78
rodrigobittencourtlima <rodrigo.bittencourt.lima@gmail.com>
2022-08-10 17:36:30 -0300

    Factory pattern and more tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,29 +4,63 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entities.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
 	@Test
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		double amount = 200.00;
-		double expectedValeu = 196.00;
-		Account acc = new Account(1L, 0.00);
+		double expectedValue = 196.00;
+		Account acc = AccountFactory.createEmptyAccount();
 
 		acc.deposit(amount);
 
-		Assertions.assertEquals(expectedValeu, acc.getBalance());
+		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
 
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
 		double amount = -200.00;
-		double expectedValeu = 100.00;
-		Account acc = new Account(1L, expectedValeu);
+		double expectedValue = 100.00;
+		Account acc = AccountFactory.createAccount(expectedValue);
 
 		acc.deposit(amount);
 
-		Assertions.assertEquals(expectedValeu, acc.getBalance());
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+		double initialBalance = 800.00;
+		double expectedValue = 0.0;
+		Account acc = AccountFactory.createAccount(initialBalance);
+
+		double result = acc.fullWithdraw();
+
+		Assertions.assertEquals(result, initialBalance);
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void withdrawShouldDecreaseBalanceWhenSufficientBalance() {
+		double initialBalance = 800.00;
+		double amount = 200.00;
+		double expectedValue = 600.00;
+		Account acc = AccountFactory.createAccount(initialBalance);
+
+		acc.withdraw(amount);
+
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+
+	@Test
+	public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Account acc = AccountFactory.createAccount(800.00);
+
+			acc.withdraw(801.00);
+		});
 	}
 
 }
